968bc56baa849a7d8d0bbf0b3f4ff74ef7c61baa
vector <lamexd097@gmail.com>
2024-10-03 04:17:24 +0530

    Adding Version 1

---
+++ Version1/src/Bishop.java
@@ -0,0 +1,43 @@
+package Version1.src;
+
+class Bishop extends Piece{
+
+    Bishop(char name, int points, char colour) {
+        super(name, points, colour);
+    }
+
+    @Override
+    public boolean moveTo(Piece[][] board, int startRow, int startCol, int endRow, int endCol) {
+
+        if ( startRow == endRow && startCol == endCol) return false;
+
+        if ( Math.abs(startCol-endCol) == Math.abs(startRow-endRow)){
+            int diff = Math.abs (startCol-endCol);
+
+            // CHECKING ALL 4 DIRECTIONS A BISHOP CAN MOVE
+            if ( startCol > endCol && startRow > endRow){
+                for (int i = 1; i< diff; i++){
+                    if (board[startRow-i][startCol-i].COLOUR != ' ') return false;
+                }
+            }
+            if ( startCol > endCol && startRow < endRow){
+                for (int i = 1; i< diff; i++){
+                    if (board[startRow+i][startCol-i].COLOUR != ' ') return false;
+                }
+            }
+            if ( startCol < endCol && startRow > endRow){
+                for (int i = 1; i< diff; i++){
+                    if (board[startRow-i][startCol+i].COLOUR != ' ') return false;
+                }
+            }
+            if ( startCol < endCol && startRow < endRow){
+                for (int i = 1; i< diff; i++){
+                    if (board[startRow+i][startCol+i].COLOUR != ' ') return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Version1/src/EndConditions.java
@@ -0,0 +1,21 @@
+package Version1.src;
+
+import java.util.Stack;
+
+public class EndConditions {
+
+    private static int moves;
+    private static boolean isKingUnsafe;
+
+    public static boolean checkMate(Piece[][] board, char player, Stack<Piece[][]> st){
+        moves = Functions.totalLegalMoves(board, player, st);
+        isKingUnsafe = Functions.isKingUnsafe(board, player);
+
+        return ( moves == 0 && isKingUnsafe)  ;
+    }
+
+    public static boolean staleMate(){
+        return  moves == 0 && !isKingUnsafe  ;
+    }
+
+}
